ca6df527f0703ee29dec2ead8946b62d2eed2f83
AlexKtf <alexandre.ktifa@gmail.com>
2017-07-05 17:38:46 +0200

    Use rack request instead of action dispatch

--- lib/lemon_way/middleware.rb
+++ lib/lemon_way/middleware.rb
@@ -7,9 +7,11 @@ module LemonWay
     end
 
     def call(env)
+      request = Rack::Request.new(env)
+
       ::RequestStore.store[:lemon_way] = {
-        ip: env['action_dispatch.remote_ip'].to_s,
-        user_agent: env['HTTP_USER_AGENT']
+        ip: request.ip,
+        user_agent: request.user_agent
       }
 
       @app.call(env)
