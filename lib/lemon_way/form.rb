4fbface9e339e4ee6f8e2be945c81a767287a9bf
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2017-07-10 20:10:40 +0200

    Change parameters to ID for FORM module

--- lib/lemon_way/form.rb
+++ lib/lemon_way/form.rb
@@ -6,15 +6,17 @@ module LemonWay
   class Form
     class << self
       def create(params = {})
-        params[:optId] = params[:id] || LemonWay::Generators::Id.generate(length: 60)
+        params[:optId] = params.delete(:id) || LemonWay::Generators::Id.generate(length: 60)
         LemonWay.client.send_request('CreatePaymentForm', '1.0', params)
       end
 
       def get(params = {})
+        params[:formId] = params.delete(:id)
         LemonWay.client.send_request('GetCompletedPaymentForm', '1.0', params)
       end
 
       def disable(params = {})
+        params[:formId] = params.delete(:id)
         LemonWay.client.send_request('DisablePaymentForm', '1.0', params)
       end
     end
