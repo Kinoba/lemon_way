31bcdae56f5f6a69f7c4542c1af09178cace8b4e
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2017-07-10 20:26:02 +0200

    Change wallet module with id paramter

--- lib/lemon_way/wallet.rb
+++ lib/lemon_way/wallet.rb
@@ -7,27 +7,32 @@ module LemonWay
   class Wallet
     class << self
       def get(params = {})
+        params[:wallet] = params.delete(:id)
         LemonWay.client.send_request('GetWalletDetails', '2.0', params)
       end
 
       def register(params = {})
-        params[:id] ||= LemonWay::Generators::Id.generate
+        params[:wallet] = params.delete(:id) || LemonWay::Generators::Id.generate
         LemonWay.client.send_request('RegisterWallet', '1.1', params)
       end
 
       def update(params = {})
+        params[:wallet] = params.delete(:id)
         LemonWay.client.send_request('UpdateWalletDetails', '1.0', params)
       end
 
       def update_status(params = {})
+        params[:wallet] = params.delete(:id)
         LemonWay.client.send_request('UpdateWalletStatus', '1.0', params)
       end
 
       def upload_file(params = {})
+        params[:wallet] = params.delete(:id)
         LemonWay.client.send_request('UploadFile', '1.1', params)
       end
 
       def transactions_history(params = {})
+        params[:wallet] = params.delete(:id)
         LemonWay.client.send_request('GetWalletTransHistory', '2.1', params)
       end
     end
